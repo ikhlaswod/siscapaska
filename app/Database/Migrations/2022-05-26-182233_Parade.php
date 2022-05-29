1951d32190b91fb101c04489b89e4c4a428d4186
Muhammad Yahya <moch.yahya95@gmail.com>
2022-05-29 14:44:07 +0700

    menambahkan entitas dan models

--- app/Database/Migrations/2022-05-26-182233_Parade.php
+++ app/Database/Migrations/2022-05-26-182233_Parade.php
@@ -8,11 +8,68 @@ class Parade extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'id_peserta' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'tinggi_badan' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'berat_badan' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'mata' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'bahu' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'tangan' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'kaki' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'platefoot' => [
+                'type' => 'INT',
+                'null' => FALSE
+            ],
+            'total_nilai' => [
+                'type' => 'double',
+                'null' => FALSE
+            ],
+            'created_at' => [
+                'type' => 'datetime',
+                'null' => TRUE
+            ],
+            'updated_at' => [
+                'type' => 'datetime',
+                'null' => TRUE
+            ],
+            'deleted_at' => [
+                'type' => 'datetime',
+                'null' => TRUE
+            ]
+        ]);
+        $this->forge->addKey('id',TRUE);
+        $this->forge->addForeignKey('id', 'peserta', 'id');
+        $this->forge->createTable('parade');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('parade');
     }
 }
--- app/Database/Migrations/2022-05-26-182619_Peserta.php
+++ app/Database/Migrations/2022-05-26-182619_Peserta.php
@@ -8,11 +8,115 @@ class Peserta extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'kode' => [
+                'type' => 'VARCHAR',
+                'constraint' => 6,
+                'null' => FALSE
+            ],
+            'nama' => [
+                'type'=>'varchar',
+                'constraint' => 50,
+                'null' => FALSE
+            ],
+            'jk' => [
+                'type' => 'varchar',
+                'constraint' => 1,
+                'null' => FALSE
+            ],
+            'ttl' => [
+                'type' => 'varchar',
+                'constraint' => 30,
+                'null' => FALSE
+            ],
+            'agama' => [
+                'type' => 'varchar',
+                'constraint' => 10,
+                'null' => FALSE
+            ],
+            'alamat' => [
+                'type' => 'text',
+                'null' => FALSE
+            ],
+            'telp' => [
+                'type' => 'varchar',
+                'constraint' => 15,
+                'null' => FALSE
+            ],
+            'hp' => [
+                'type' => 'varchar',
+                'constraint' => 13,
+                'null' => FALSE
+            ],
+            'asal_sekolah' => [
+                'type' => 'varchar',
+                'constraint' => 50,
+                'null' => FALSE
+            ],
+            'tinggi_badan' => [
+                'type' => 'int',
+                'constraint' => 3,
+                'null' => FALSE
+            ],
+            'berat_badan' => [
+                'type' => 'int',
+                'constraint' => 3,
+                'null' => FALSE
+            ],
+            'golongan_darah' => [
+                'type' => 'varchar',
+                'constraint' => 2,
+                'null' => FALSE
+            ],
+            'nama_ayah' => [
+                'type' => 'varchar',
+                'constraint' => 50,
+                'null' => FALSE
+            ],
+            'nama_ibu' => [
+                'type' => 'varchar',
+                'constraint' => 50,
+                'null' => FALSE
+            ],
+            'pekerjaan_ayah' => [
+                'type' => 'varchar',
+                'constraint' => 50,
+                'null' => FALSE
+            ],
+            'pekerjaan_ibu' => [
+                'type' => 'varchar',
+                'constraint' => 50,
+                'null' => FALSE
+            ],
+            'alamat_ortu' => [
+                'type' => 'text',
+                'null' => FALSE
+            ],
+            'created_at' => [
+                'type' => 'datetime',
+                'null' => TRUE
+            ],
+            'updated_at' => [
+                'type' => 'datetime',
+                'null' => TRUE
+            ],
+            'deleted_at' => [
+                'type' => 'datetime',
+                'null' => TRUE
+            ]
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('peserta');
+
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('peserta');
     }
 }
